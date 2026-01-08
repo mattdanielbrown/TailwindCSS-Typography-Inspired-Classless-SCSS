95a03bd0aee19731382aad891c12921036f19169
mattdanielbrown <mattbrown@onmail.com>
2026-01-08 15:56:39 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+import 'sanitize.css';
+import 'modern-normalize';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/400-italic.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/500-italic.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/600-italic.css';
+import '../css/style.scss';
---
+++ webpack.common.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './js/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: './js/app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          // Compiles Sass to CSS, resolves imports/url(), and injects CSS at runtime.
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|avif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      },
+    ],
+  },
+};
